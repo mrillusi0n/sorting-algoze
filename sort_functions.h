4c31c7c70a8c86fba5e26fa7766cb6ecd0a04822
Nikhil CSB <csbnikhil@gmail.com>
2020-09-18 23:15:15 +0530

    defines the interface

---
+++ sort_functions.h
@@ -0,0 +1,17 @@
+/********************************
+ * Author: theteachr
+ *******************************/ 
+
+#ifndef _SORT_FUNCTIONS__
+#define _SORT_FUNCTIONS__
+
+void bubble_sort(int *, int);
+void selection_sort(int *, int);
+void insertion_sort(int *, int);
+void merge_sort(int *, int);
+void quick_sort(int *, int);
+void bucket_sort(int *, int);
+void counting_sort(int *, int);
+void radix_sort(int *, int);
+
+#endif
