8e4d72d96cd50dd0a7cb8e5be713721890acd72a
Nikhil CSB <csbnikhil@gmail.com>
2020-09-18 23:14:36 +0530

    implements selection_sort

---
+++ src/selection_sort.c
@@ -0,0 +1,17 @@
+void selection_sort(int *nums, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int min_index = i;
+
+		for (int j = i; j < size; j++)
+		{
+			if (nums[min_index] > nums[j])
+				min_index = j;
+		}
+
+		int temp = nums[i];
+		nums[i] = nums[min_index];
+		nums[min_index] = temp;
+	}
+}
