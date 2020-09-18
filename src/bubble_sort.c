48c24f4993d6af11475f7e1b7a348af2654f5f58
Nikhil CSB <csbnikhil@gmail.com>
2020-09-18 15:41:20 +0530

    implements bubble_sort

---
+++ src/bubble_sort.c
@@ -0,0 +1,15 @@
+void bubble_sort(int *nums, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (nums[j] > nums[j + 1])
+			{
+				int temp = nums[j];
+				nums[j] = nums[j + 1];
+				nums[j + 1] = temp;
+			}
+		}
+	}
+}
