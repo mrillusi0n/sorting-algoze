f3688547e414e91852246ef042c25464a4edad9a
Nikhil CSB <csbnikhil@gmail.com>
2020-09-26 22:51:31 +0530

    implement insertion_sort

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,5 +1,15 @@
+extern void swap(int *, int *);
+
 void insertion_sort(int *nums, int size) {
 	for (int i = 1; i < size; i++) {
-		
+		int curr = nums[i];
+		int j = i - 1;
+
+		while (j >= 0 && nums[j] > curr) {
+			nums[j + 1] = nums[j];
+			j--;
+		}
+
+		nums[j + 1] = curr;
 	}
 }
