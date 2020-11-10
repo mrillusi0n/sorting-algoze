00f251f74def1c8cb058dccdc2d86cd5468c63c2
Nikhil CSB <csbnikhil@gmail.com>
2020-11-10 16:59:37 +0530

    got it working

--- src/merge_sort.c
+++ src/merge_sort.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /* internal routines */
 static void _merge_sort(int *, int, int);
 static void _merge(int *, int, int, int);
 static void _copy(int *, int *, int);
-static int* _get_memory(int);
 
 void merge_sort(int *nums, int size) {
 	_merge_sort(nums, 0, size - 1);
 }
 
 static void _merge_sort(int *nums, int left, int right) {
-	if (left >= right) return;
+	if (left == right)
+		return;
 
 	int mid = left + (right - left) / 2;
 	_merge_sort(nums, left, mid);
@@ -20,36 +21,30 @@ static void _merge_sort(int *nums, int left, int right) {
 }
 
 static void _merge(int *nums, int left, int mid, int right) {
-	int i = 0;
-	int j = 0;
-	int k = left;
+	int i = 0, j = 0, k = left;
 
 	int n_one = mid - left + 1;
 	int n_two = right - mid;
 
-	int *one = _get_memory(n_one);
-	int *two = _get_memory(n_two);
+	int left_arr[n_one], right_arr[n_two];
 
-	_copy(nums + left, one, n_one);
-	_copy(nums + mid + 1, two, n_two);
+	_copy(nums + left, left_arr, n_one);
+	_copy(nums + mid + 1, right_arr, n_two);
+
 
 	while (i < n_one && j < n_two) {
-		if (one[i] < two[j])
-			nums[k] = one[i++];
+		if (left_arr[i] < right_arr[j])
+			nums[k] = left_arr[i++];
 		else
-			nums[k] = two[j++];
+			nums[k] = right_arr[j++];
 		k++;
 	}
 
-	_copy(one, nums + k, n_one - i);
-	_copy(two, nums + k, n_two - j);
+	_copy(left_arr + i, nums + k, n_one - i);
+	_copy(right_arr + j, nums + k, n_two - j);
 }
 
 static void _copy(int *src, int *dest, int count)  {
 	for (int i = 0; i < count; i++)
 		dest[i] = src[i];
-}
-
-static int* _get_memory(int size) {
-	return malloc(sizeof(int) * size);
 }
